87d0d772d943e338f2591b5101cd06733f9aff3f
Mahmoud062 <mahmoudsolimanv@gmail.com>
2026-04-07 01:22:41 +0200

    Day 1 Complete: Project setup, configured Tailwind & PostCSS, built styled Navbar and Footer

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLists = ["Store", "Mac", "iPhone", "Support"];
+
+export const footerLinks = [
+  "Privacy Policy",
+  "Terms of Service",
+  "Cookie Settings",
+  "Education",
+];
---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+// Image imports (placeholder structure - will be populated with actual assets)
+export const appleImg = "/assets/images/apple.svg";
+export const searchImg = "/assets/images/search.svg";
+export const bagImg = "/assets/images/bag.svg";
